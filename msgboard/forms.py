6ad45814b3c4536e43f9fc73fe9545f4784055f1
Yuval-Vino <99268065+Yuval-Vino@users.noreply.github.com>
2022-02-14 18:04:42 +0200

    Update forms.py

--- msgboard/forms.py
+++ msgboard/forms.py
@@ -3,7 +3,7 @@ from .models import Message
 
 
 class MessageForm(forms.ModelForm):
-   class Meta:
-          model = Message
-          fields = ('author', 'text')
+    class Meta:
+        model = Message
+        fields = ('author', 'text')
             
